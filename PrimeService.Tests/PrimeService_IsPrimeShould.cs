858a674dba0e26737f091cb7ad0b9d75b5a08286
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2021-06-21 12:02:17 +0300

    First commit

---
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prime.UnitTests.Services.UnitTests.Services;
+
+namespace Prime.UnitTests.Services
+{
+    [TestClass]
+    public class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [TestMethod]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+            bool result = _primeService.IsPrime(1);
+
+            Assert.IsFalse(result, "1 should not be prime");
+        }
+    }
+}
